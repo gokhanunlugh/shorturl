a12c59c6026dfbd1f066f8dec2772dca226ba5de
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-09 20:03:33 +0300

    form oluşturuldu

---
+++ app/form.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Form(){
+  
+  return (
+    <form action="">
+    <input type="text" name="url" placehlder="Linki giriniz" />
+    <button>Linki Kısalt</button>
+    </form>
+  )
+}
