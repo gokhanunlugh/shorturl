72a03d5ba674dbe0445d0069df14b2ad820be65c
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 14:53:37 +0300

    fetch id den sharturl e çevirildi

--- app/[url]/page.jsx
+++ app/[url]/page.jsx
@@ -4,7 +4,7 @@ import { notFound, redirect } from "next/navigation";
 
 const  DynamicUrlPage = async ({params}) => {
 
-  const response = await fetch(`https://iblitesiugyvrzmeoqsf.supabase.co/rest/v1/urls?id=eq.${params.url}&select=*`,{
+  const response = await fetch(`https://iblitesiugyvrzmeoqsf.supabase.co/rest/v1/urls?short_url=eq.${params.url}&select=*`,{
   method: "GET",
   headers: defaultHeader
 
