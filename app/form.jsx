1d74ef19716fefcb7c1347ec613f0a94397d5042
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 14:06:35 +0300

    toast, formref eklendi

--- app/form.jsx
+++ app/form.jsx
@@ -1,19 +1,32 @@
 "use client"
 import { linkShortener } from "@/actions/link"
 import { useFormState } from "react-dom"
+import { useEffect, useRef } from "react"
+import { toast } from "sonner"
 
 
-export default function Form(){
+export default function Form() {
 
   const [state, shorten] = useFormState(linkShortener, {
     message: null,
     error: null
   })
-  
+
+  const formRef = useRef(null);
+  useEffect(() => {
+    if (state?.message) {
+      toast.success(state.message)
+      formRef.current.reset();
+    }
+    if (state?.error) {
+      toast.error(state.error)
+    }
+  }, [state])
+
   return (
-    <form action={shorten}>
-    <input type="text" name="url" placehlder="Linki giriniz" />
-    <button>Linki Kısalt</button>
+    <form ref={formRef} action={shorten}>
+      <input type="text" name="url" placehlder="Linki giriniz" />
+      <button>Linki Kısalt</button>
     </form>
   )
 }
