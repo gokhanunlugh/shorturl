d60309a214f0ff89b3cfda3d57455bac63af93a1
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 15:02:18 +0300

    login-signup için header oluşturuldu

---
+++ components/Header/page.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Header(){
+  return(
+    <header>
+      <form action="">
+        <input type="email" name="email" />
+        <input type="password" name="pass" />
+        <button>Giriş</button>
+      </form>
+      <button>Üye Ol</button>
+    </header>
+  )
+}
